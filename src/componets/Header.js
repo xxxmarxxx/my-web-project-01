148932481f96559779b89dae659750cd01eb00c7
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-12 18:06:08 +0100

    create header

---
+++ src/componets/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../styles/Mariusz.css";
+import "../styles/Mariusz.css";
+
+const Header = () => {
+  return (
+    <header class="header">
+      <div class="banner">
+        <h3 class="banner-subtitle">Photography</h3>
+        <h1 class="banner-title">my Gallery</h1>
+        <a href="./images/FinalesZertifikat_FbW25-2.pdf" class="banner-btn">
+          See Zertifikat
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;

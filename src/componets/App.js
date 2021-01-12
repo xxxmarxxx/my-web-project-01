191f601104504b8a4a23f9cf5ceea4b6651c204a
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-12 18:16:45 +0100

    create app.js

--- src/componets/App.js
+++ src/componets/App.js
@@ -1,12 +1,29 @@
+import React from "react";
+// import ReactDOM from "react-dom";
+import Footer from "./Footer";
+import Contact from "./Contact";
+import Gallery from "./Gallery";
+import About from "./AboutMe";
+import Skill from "./Skills";
+import Header from "./Header";
+import Nav from "./Nav";
 
 import '../styles/App.css';
 
 function App() {
   return (
     <div className="App">
-    <h1>hallo world</h1>
+
+      <Nav />
+      <Header />
+      <Skill />
+      <About />
+      <Gallery />
+      <Contact />
+      <Footer />
     </div>
   );
 }
 
 export default App;
+

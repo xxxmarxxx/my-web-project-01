04179872d9f60cf19e05b75b43159cceb3329780
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-14 15:25:52 +0100

    cleaning

--- src/componets/Skills.js
+++ src/componets/Skills.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import '../styles/Mariusz.css';
-import { FaLightbulb, FaDesktop, FaCameraRetro, FaCamera } from "react-icons/fa";
-
+import React from "react";
+import "../styles/Mariusz.css";
+import {
+  FaLightbulb,
+  FaDesktop,
+  FaCameraRetro,
+  FaCamera,
+} from "react-icons/fa";
 
 const Skills = () => {
-    return ( 
-
-        <section class="skills clearfix">
+  return (
+    <section class="skills clearfix">
       {/* <!-- single skill --> */}
       <article class="skill">
         <span class="skill-icon">
-        <FaCamera className="social-icon2"/>
+          <FaCamera className="social-icon2" />
         </span>
         <h3 class="skill-title">street photo</h3>
         {/* <!-- ten words --> */}
@@ -20,8 +23,8 @@ const Skills = () => {
       <!-- single skill --> */}
       <article class="skill">
         <span class="skill-icon">
-        <FaLightbulb className="social-icon2"/> 
-            </span>
+          <FaLightbulb className="social-icon2" />
+        </span>
         <h3 class="skill-title">light painting</h3>
         {/* <!-- ten words --> */}
         <p class="skill-text">We use highest quality.</p>
@@ -30,7 +33,7 @@ const Skills = () => {
       <!-- single skill --> */}
       <article class="skill">
         <span class="skill-icon">
-        <FaCameraRetro className="social-icon2"/>
+          <FaCameraRetro className="social-icon2" />
           <i class="fas fa-camera-retro"></i>
         </span>
         <h3 class="skill-title">street photo</h3>
@@ -41,7 +44,7 @@ const Skills = () => {
       <!-- single skill --> */}
       <article class="skill">
         <span class="skill-icon">
-        <FaDesktop className="social-icon2"/>
+          <FaDesktop className="social-icon2" />
           <i class="fas fa-desktop"></i>
         </span>
         <h3 class="skill-title">street photo</h3>
@@ -50,7 +53,7 @@ const Skills = () => {
       </article>
       {/* <!-- end of single skill --> */}
     </section>
-     );
-}
- 
+  );
+};
+
 export default Skills;
